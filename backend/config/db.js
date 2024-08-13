78d19b22113f275ecc3c9a7f61449b9244bc02de
Anisha <anishaanuu308@gmail.com>
2024-08-13 23:11:11 +0530

    configured react,node,tailwind css

---
+++ backend/config/db.js
@@ -0,0 +1,3 @@
+import pg from 'pg'
+
+const pg=new pg({})
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Layout from './Layout/layout.jsx';
+function App() {
+  return (
+   <div><Layout/></div>
+  );
+}
+
+export default App;
